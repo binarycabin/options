5953b59cf0cf95ddf0fc17f369ace3b6bcefd901
Jeff Kilroy <jeffkilroy@gmail.com>
2019-03-08 03:08:10 +0000

    Apply fixes from StyleCI

--- tests/CommandMakeOptionTest.php
+++ tests/CommandMakeOptionTest.php
@@ -2,12 +2,11 @@
 
 namespace BinaryCabin\Options\Tests;
 
-use Illuminate\Support\Facades\Artisan;
 use Orchestra\Testbench\TestCase;
+use Illuminate\Support\Facades\Artisan;
 
 class CommandMakeOptionTest extends TestCase
 {
-
     protected function getPackageProviders($app)
     {
         return [\BinaryCabin\Options\Providers\OptionServiceProvider::class];
@@ -16,15 +15,14 @@ class CommandMakeOptionTest extends TestCase
     public function test_make_option_creates_option_file()
     {
         $generatedFileLocation = app_path().'/Options/Countries.php';
-        if(file_exists($generatedFileLocation)){
+        if (file_exists($generatedFileLocation)) {
             unlink($generatedFileLocation);
         }
         $this->withoutMockingConsoleOutput();
         $this->artisan('make:option Countries');
         $output = Artisan::output();
-        $this->assertContains('Done!',$output);
+        $this->assertContains('Done!', $output);
         $this->assertTrue(file_exists($generatedFileLocation));
         unlink($generatedFileLocation);
     }
-
 }
